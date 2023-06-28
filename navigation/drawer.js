074acaa0bc5dcd5580801e6c4e404de05ccc4e38
Rydhmi <87961243+Rydhmi@users.noreply.github.com>
2023-06-28 14:38:49 +0530

    drawerNavigation

--- navigation/drawer.js
+++ navigation/drawer.js
@@ -1,14 +1,16 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import EventDetailScreen from "../screens/event-detail";
-import HomeScreen from "../screens/home_screen";
+// import EventDetailScreen from "../screens/event-detail";
+// import HomeScreen from "../screens/home_screen";
 import { HomeStack } from "./stack";
 
 const Drawer = createDrawerNavigator();
 
 export const MyDrawer = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={HomeStack} />
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen name="HomeStack" component={HomeStack} options={{
+        title:'Home'
+      }}/>
       {/* <Drawer.Screen name="Event" component={EventDetailScreen} /> */}
     </Drawer.Navigator>
   );
--- navigation/stack.js
+++ navigation/stack.js
@@ -1,12 +1,15 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/home_screen";
 import EventDetailScreen from "../screens/event-detail";
+import { navOptions } from "./options";
+import { useNavigation } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
 export const HomeStack = () => {
+  const navigation = useNavigation();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={() => navOptions(navigation)}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="EventDetail" component={EventDetailScreen} />
     </Stack.Navigator>
