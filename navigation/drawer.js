4e23c4f28e872821fafdb39444374c38e489a9aa
Rydhmi <87961243+Rydhmi@users.noreply.github.com>
2023-06-29 10:05:25 +0530

    drawer update

--- navigation/drawer.js
+++ navigation/drawer.js
@@ -1,20 +1,20 @@
+import React from "react";
 import {
   DrawerItem,
   DrawerItemList,
   createDrawerNavigator,
 } from "@react-navigation/drawer";
-import EventDetailScreen from "../screens/event-detail";
-import HomeScreen from "../screens/home_screen";
-import { HomeStack, ProfileStack } from "./stack";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image, View, StyleSheet, Linking } from "react-native";
+import { Image, View, Linking } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+
+import HomeStack from "./stack/HomeStack";
+import ProfileStack from "./stack/ProfileStack";
 
 const Drawer = createDrawerNavigator();
 
-export const MyDrawer = () => {
+const MyDrawer = () => {
   return (
-<<<<<<< HEAD
     <Drawer.Navigator
       drawerContent={(props) => {
         return (
@@ -48,7 +48,6 @@ export const MyDrawer = () => {
     >
       <Drawer.Screen
         name="HomeStack"
-        // style={styles.text}
         component={HomeStack}
         options={{
           title: "Home",
@@ -63,20 +62,8 @@ export const MyDrawer = () => {
           drawerIcon: () => <Ionicons name="person-outline" size={22} />,
         }}
       />
-      {/* <Drawer.Screen name="Event" component={EventDetailScreen} /> */}
-=======
-    <Drawer.Navigator screenOptions={{headerShown: false}}>
-      <Drawer.Screen name="HomeStack" component={HomeStack}options={{
-        title:'Home'
-      }} />
-      {/* {<Drawer.Screen name="Event" component={EventDetailScreen} />} */}
->>>>>>> eacb2d134ce29b3639b6de64f2e54f3c9a3fde4a
     </Drawer.Navigator>
   );
 };
 
-// const styles = StyleSheet.create({
-//   text: {
-//     backgroundColor: "white",
-//   },
-// });
+export default MyDrawer;
